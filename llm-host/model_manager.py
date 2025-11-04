dd2bc1316571187ec5cafb5455e1d40dcbca2187
Gil Carelli <gmcarelli@gmail.com>
2025-11-04 14:35:25 -0300

    Create model_manager.py

---
+++ llm-host/model_manager.py
@@ -0,0 +1,12 @@
+from typing import List
+from llm_tools import LLMTools
+
+class ModelManager:
+    def __init__(self, tools: LLMTools):
+        self.tools: LLMTools = tools
+        
+    def create_custom_model(self, base_model: str, model_name: str) -> bool:
+        return self.tools.create_model(base_model, model_name)
+
+    def list_models(self) -> List:
+        return self.tools.list_models()
